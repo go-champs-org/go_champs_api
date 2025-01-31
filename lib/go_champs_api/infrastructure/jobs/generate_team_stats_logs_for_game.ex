15a2a93af49fd213b3c5c9c886582302c75cd6a0
Lair Junior <lairjr@outlook.com>
2025-01-31 16:07:38 -0300

    Call function from job

--- lib/go_champs_api/infrastructure/jobs/generate_team_stats_logs_for_game.ex
+++ lib/go_champs_api/infrastructure/jobs/generate_team_stats_logs_for_game.ex
@@ -1,8 +1,17 @@
 defmodule GoChampsApi.Infrastructure.Jobs.GenerateTeamStatsLogsForGame do
   use Oban.Worker, queue: :generate_team_stats_logs_for_game
 
+  alias GoChampsApi.TeamStatsLogs
+
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"game_id" => game_id}}) do
-    IO.inspect("Generating team stats logs for game #{game_id}")
+    case game_id do
+      nil ->
+        {:error, "game_id is required"}
+
+      _ ->
+        TeamStatsLogs.generate_team_stats_log_from_game_id(game_id)
+        {:ok, "Team stats logs generated for game #{game_id}"}
+    end
   end
 end
