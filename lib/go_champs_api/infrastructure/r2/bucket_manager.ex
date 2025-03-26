49c2ebbdddd4fda7bfdaa2d84fbde514b2b1714f
Lair Junior <lairjr@outlook.com>
2025-03-26 17:41:19 -0300

    Allow upload to multiple buckets

---
+++ lib/go_champs_api/infrastructure/r2/bucket_manager.ex
@@ -0,0 +1,40 @@
+defmodule GoChampsApi.Infrastructure.R2.BucketManager do
+  def select_bucket("registration-consents"), do: "registration-consents"
+  def select_bucket("team-logos"), do: "team-logos"
+  def select_bucket(nil), do: "registration-consents"
+
+  def generate_presigned_upload_url(bucket, filename, content_type) do
+    config = ExAws.Config.new(:s3)
+
+    unique_filename = "#{Ecto.UUID.generate()}_#{filename}"
+
+    {:ok, presigned_url} =
+      ExAws.S3.presigned_url(
+        config,
+        :put,
+        bucket,
+        "uploads/#{unique_filename}",
+        # 1 hour expiration
+        expires_in: 3600,
+        content_type: content_type
+      )
+
+    public_url = generate_public_url(bucket, unique_filename)
+
+    {:ok, presigned_url, public_url}
+  end
+
+  def generate_public_url(bucket, filename) do
+    "https://#{bucket}.go-champs.com/uploads/#{filename}"
+  end
+
+  def upload_file(bucket, filename, content) do
+    ExAws.S3.put_object(bucket, "uploads/#{filename}", content)
+    |> ExAws.request()
+  end
+
+  def delete_file(bucket, filename) do
+    ExAws.S3.delete_object(bucket, "uploads/#{filename}")
+    |> ExAws.request()
+  end
+end
--- lib/go_champs_api_web/controllers/upload_controller.ex
+++ lib/go_champs_api_web/controllers/upload_controller.ex
@@ -1,27 +1,47 @@
 defmodule GoChampsApiWeb.UploadController do
   use GoChampsApiWeb, :controller
 
-  def presigned_url(conn, %{"filename" => filename, "content_type" => content_type}) do
+  alias GoChampsApi.Infrastructure.R2.BucketManager
+
+  def presigned_url(conn, %{
+        "filename" => filename,
+        "content_type" => content_type,
+        "file_type" => file_type
+      }) do
     with {:ok, _} <- validate_file_size(conn.params),
          {:ok, _} <- validate_file_type(content_type),
          {:ok, presigned_url, public_url} <-
-           GoChampsApi.Infrastructure.R2.RegistrationConsents.generate_presigned_upload_url(
-             filename,
-             content_type
-           ) do
+           BucketManager.select_bucket(file_type)
+           |> BucketManager.generate_presigned_upload_url(filename, content_type) do
       json(conn, %{data: %{filename: filename, url: presigned_url, public_url: public_url}})
     else
       {:error, reason} -> handle_error(conn, reason)
     end
   end
 
-  def delete_file(conn, %{"url" => url}) do
+  def presigned_url(conn, %{
+        "filename" => filename,
+        "content_type" => content_type
+      }) do
+    with {:ok, _} <- validate_file_size(conn.params),
+         {:ok, _} <- validate_file_type(content_type),
+         {:ok, presigned_url, public_url} <-
+           BucketManager.select_bucket("registration-consents")
+           |> BucketManager.generate_presigned_upload_url(filename, content_type) do
+      json(conn, %{data: %{filename: filename, url: presigned_url, public_url: public_url}})
+    else
+      {:error, reason} -> handle_error(conn, reason)
+    end
+  end
+
+  def delete_file(conn, %{"url" => url, "file_type" => file_type}) do
     filename =
       String.split(url, "/")
       |> List.last()
       |> URI.decode()
 
-    case GoChampsApi.Infrastructure.R2.RegistrationConsents.delete_file(filename) do
+    case BucketManager.select_bucket(file_type)
+         |> BucketManager.delete_file(filename) do
       {:ok, _} -> send_resp(conn, :no_content, "")
       {:error, reason} -> handle_error(conn, reason)
     end
